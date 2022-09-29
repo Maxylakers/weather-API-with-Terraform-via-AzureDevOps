0a75302f20ff8bd8d0824bb9dee99a0d0d9de99a
Revolver <maxylakers@gmail.com>
2022-09-29 10:27:02 +0100

    feat: add infrastructure code using Terraform

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "tfmax_rg_blobstore"
+#     storage_account_name = "tfmaxstorage"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name     = "max-rg"
+  location = "Central Us"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+  ip_address_type = "Public"
+  dns_name_label  = "maxcontainer"
+  os_type         = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "scorpionrevolver/weatherapi:${var.imagebuild}"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
